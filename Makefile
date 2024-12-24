3c6e5609bfd467e562ee3db48174f364fdbf1766
fmixolydian <fmixolydian@proton.me>
2024-12-24 11:33:53 +0100

    Added API Jumptable

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ xenon: src/*.c bios.bin
 	clang -O3 -g src/*.c -o xenon -l6502 -lraylib -lm -pedantic -Wall -Wno-overflow -pg
 
 build/%.obj: bios/%.s
-	ca65 --cpu 6502 --verbose $< -o $@ --listing {$@:obj=lst}
+	ca65 --cpu 6502 --verbose $< -o $@
 	
 bios.bin: build/bios.obj
 	ld65 -v -o bios.bin build/*.obj -m bios.map -C conf/xenon_bios.ld
--- bios/api.s
+++ bios/api.s
@@ -4,7 +4,24 @@ org $F800
 ;a = function number
 api:
 	pushall
+	
+	sta z_addr
+	lda #$FF
+	sta z_addr + 1
+	
+	jmp (z_addr)
+	
+null:
+	rts
+
+memcpy:
+	lda #'a'
+	sta IO_CON
+	
 	popall
 	rts
 
+org $FF00
 
+api_table:
+	.word null,   null,   null,   null,   memcpy, null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null
